dc1f896a63827f25fcba950a041817d4178c4ecd
MartasHM <martasholisticmassage@gmail.com>
2025-10-24 19:33:11 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Martasholisticmassage-site/',  // ✅ Add this line
   plugins: [react()],
+  base: '/Martasholisticmassage-site/'
 })
